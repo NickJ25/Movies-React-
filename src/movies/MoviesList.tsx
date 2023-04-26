4a1d6b669bf623ec15ecb6ac06ab00af205a66c7
NickJ25 <35298197+NickJ25@users.noreply.github.com>
2023-04-26 15:12:24 -0800

    Decoupled lists

--- src/movies/MoviesList.tsx
+++ src/movies/MoviesList.tsx
@@ -1,25 +1,23 @@
+import GenericList from "../utils/GenericList";
 import Loading from "../utils/Loading";
 import IndividualMovie from "./IndividualMovie";
 import { movieDTO } from "./movies.model";
-import css from "./MoviesList.module.css"
+import css from "./MoviesList.module.css";
 
-interface moviesListProps{
-    movies?: movieDTO[];
+interface moviesListProps {
+  movies?: movieDTO[];
 }
 
-function MoviesList(props: moviesListProps){
-
-    if(!props.movies){
-        return <Loading/>
-    } else if (props.movies.length === 0){
-        return <>There are no movies to displayPartsToString.</>
-    } else {
-        return (
-            <div className={css.div}>
-                {props.movies.map(movie => <IndividualMovie {...movie} key={movie.id}/>)}
-            </div>
-        );
-    }
+function MoviesList(props: moviesListProps) {
+  return (
+    <GenericList list={props.movies}>
+      <div className={css.div}>
+        {props.movies?.map((movie) => (
+          <IndividualMovie {...movie} key={movie.id} />
+        ))}
+      </div>
+    </GenericList>
+  );
 }
 
 export default MoviesList;
