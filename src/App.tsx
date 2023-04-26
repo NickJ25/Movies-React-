2fcd8e0a920e287c11c3c72877b958138a6042bc
NickJ25 <35298197+NickJ25@users.noreply.github.com>
2023-04-26 13:10:05 -0800

    Added individual movie component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import IndividualMovie from "./movies/IndividualMovie";
+import movieDTO from "./movies/movies.model";
 
 function App() {
 
+  const testMovie: movieDTO = {
+    id: 1,
+    title: 'Spider-Man: Far From Home',
+    poster: 'https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg'
+  }
 
   return (
     <>
-      <h2> Hello World</h2>
+      <IndividualMovie {...testMovie}/>
     </>
   );
 }
---
+++ src/movies/movies.model.d.ts
@@ -0,0 +1,8 @@
+interface movieDTO {
+    id: number;
+    title: string;
+    poster: string;
+}
+
+export default movieDTO;
+
